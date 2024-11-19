1011996325105303551aa81cfb8dca1d58533585
Codaerus <christianqc91@gmail.com>
2024-11-18 22:37:50 -0500

    Update and rename ejemplo1.py to imagenes.py

--- CLASE1/imagenes.py
+++ CLASE1/imagenes.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
 
-#help(cv2.imread)
+help(cv2.imwrite)
 z = cv2.imread("mario.jpg",0) #np.zeros((400,400), dtype = np.uint8)
 print(z)
 cv2.imshow("ventana",z)
 cv2.waitKey(0)
+cv2.imwrite("mario1.jpg",z)
 #print(x)
 
 #zeros(shape, dtype=float, order='C', *, like=None)->array
 #imshow(winname, mat) -> None
 #waitKey([, delay]) -> retval
 #imread(filename[, flags]) -> retval
+#imwrite(filename, img[, params]) -> retval
