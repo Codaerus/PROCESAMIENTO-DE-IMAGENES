d5434a4a090a83ca1a912f74c77067fb89a34ec4
Codaerus <christianqc91@gmail.com>
2024-12-16 22:41:45 -0500

    Create aproximate_contours.py

---
+++ CLASE10/aproximate_contours.py
@@ -0,0 +1,25 @@
+import cv2
+
+help(cv2.findContours)
+image_as_BGR = cv2.imread("image_2.png")
+image_as_GRAY = cv2.cvtColor(image_as_BGR,cv2.COLOR_BGR2GRAY)
+
+valor_umbral = 127
+valor_maximo = 255
+tipo = cv2.THRESH_BINARY
+
+ret, image_with_threshold = cv2.threshold(image_as_GRAY,valor_umbral,valor_maximo,tipo)
+contours,hierarchy  = cv2.findContours(image_with_threshold, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+perimetro = cv2.arcLength(contours[0],True)#cv2.contourArea(contours[0]) #cv2.moments(contours[0])
+distance = 0.1*perimetro
+approximate_contour = cv2.approxPolyDP(contours[0],distance,True)
+
+cv2.drawContours(image_as_BGR,[approximate_contour],-1,(0,0,255),2)
+cv2.imshow("Imagen",image_as_BGR)
+cv2.imshow("Imagen umbralizada", image_with_threshold)
+cv2.waitKey()
+
+print(contours)
+
+#findContours(image, mode, method[, contours[, hierarchy[, offset]]]) -> contours, hierarchy
