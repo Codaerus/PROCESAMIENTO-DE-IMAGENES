790853ea97f8b4eab13935d8c013d16c76a337c7
Codaerus <christianqc91@gmail.com>
2024-12-16 22:13:13 -0500

    Create area.py

---
+++ CLASE10/area.py
@@ -0,0 +1,24 @@
+import cv2
+
+help(cv2.findContours)
+image_as_BGR = cv2.imread("image_2.png")
+image_as_GRAY = cv2.cvtColor(image_as_BGR,cv2.COLOR_BGR2GRAY)
+
+valor_umbral = 127
+valor_maximo = 255
+tipo = cv2.THRESH_BINARY
+
+ret, image_with_threshold = cv2.threshold(image_as_GRAY,valor_umbral,valor_maximo,tipo)
+contours,hierarchy  = cv2.findContours(image_with_threshold, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+print("Contornos")
+print(contours)
+moments = cv2.contourArea(contours[0]) #cv2.moments(contours[0])
+print(moments)
+cv2.drawContours(image_as_BGR,contours,-1,(255,0,0),2)
+cv2.imshow("Imagen",image_as_BGR)
+cv2.imshow("Imagen umbralizada", image_with_threshold)
+cv2.waitKey()
+
+print(contours)
+
+#findContours(image, mode, method[, contours[, hierarchy[, offset]]]) -> contours, hierarchy
