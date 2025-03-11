c16af6b3d66c8223bcd16f5883d05ae2e9acb69e
Codaerus <christianqc91@gmail.com>
2025-03-10 21:58:52 -0500

    Update videos.py

--- CLASE1/videos.py
+++ CLASE1/videos.py
@@ -1,23 +1,32 @@
 import cv2
 
-help(cv2.VideoCapture)
+#help(cv2.VideoCapture)
 
 cap = cv2.VideoCapture(0)
+
 while cap.isOpened():
 	ret, frame = cap.read()
-	if ret:
-		cv2.imshow("ventana video", frame)
+	if ret == True:
+		cv2.imshow("Ventana",frame)
 		val = cv2.waitKey(1)
-		if val == 97:
+		if val & 0xFF == ord("a"):
 			break
-		if val == 98:
+		if val & 0xFF == ord("b"):
 			cv2.imwrite("img1.jpg",frame)
 	else:
 		break
 cap.release()
 cv2.destroyAllWindows()
 
+"""
+isOpened(...)
+ |      isOpened() -> retval
+ |      .   @brief Returns true if video capturing has been initialized already.
+read(...)
+ |      read([, image]) -> retval, image
+ |      .   @brief Grabs, decodes and returns the next video frame.
+release(...)
+ |      release() -> None
+ |      .   @brief Closes video file or capturing device.
 
-#isOpened() -> retval    Returns true if video capturing has been initialized already.
-#read([, image]) -> retval, image  Decodes and returns the next video frame.
-#release() -> None  Closes video file or capturing device.
+"""
