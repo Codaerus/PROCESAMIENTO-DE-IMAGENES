2b10bf813e8039933d59f6eba3f57ed0518e1719
Codaerus <christianqc91@gmail.com>
2025-03-10 22:24:21 -0500

    Update linea2.py

--- CLASE2/linea2.py
+++ CLASE2/linea2.py
@@ -1,17 +1,13 @@
-#Dibujar Linea
-
-import numpy as np
 import cv2
+import numpy as np 
 
-#help(cv2.line)
-
-z =  cv2.imread("mario.jpg",1) #np.zeros((400,400,3),dtype = np.uint8)
-print(z.shape, z.ndim, z.dtype)
-cv2.line(z,(0,0),(255,255),(0,0,255),4)
-print(z)
-cv2.imshow("Ventana de negro",z)
+help(cv2.line)
+z = np.zeros((200,400,3), dtype = np.uint8)
+h,w,c = z.shape
+print(h,w)
+print(h,w,c)
+cv2.line(z,(0,0),(w-1,h-1),(0,255,0),4) #BGR
+cv2.imshow("VENTANA", z)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
-
 
 #line(img, pt1, pt2, color[, thickness[, lineType[, shift]]]) -> img
