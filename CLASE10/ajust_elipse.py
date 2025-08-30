c8499a05e1f3bf637ea7d2d9edbfed5f4ce28b85
Codaerus <christianqc91@gmail.com>
2025-08-29 20:06:35 -0500

    Create ajust_elipse.py

---
+++ CLASE10/ajust_elipse.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+image_as_BGR = cv2.imread("image_2.png")
+image_as_GRAY = cv2.cvtColor(image_as_BGR, cv2.COLOR_BGR2GRAY)
+
+valor_umbral = 127
+valor_maximo = 255
+tipo = cv2.THRESH_BINARY
+ret, image_with_threshold = cv2.threshold(image_as_GRAY,valor_umbral, valor_maximo, tipo)
+contours, hierarchy = cv2.findContours(image_with_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+###################### Circulo Minimo de inclusión #########################
+(x,y), radius = cv2.minEnclosingCircle(contours[0])
+
+center = (int(x), int(y))
+radius = int(radius)
+color = (0,255,0)
+###################### Ajuste a Elipse #####################################
+ellipse = cv2.fitEllipse(contours[0]) #(center_x,center_y), (major_axis,minor_axis),angle
+cv2.ellipse(image_as_BGR, ellipse, color,2)
+help(cv2.ellipse)
+############################################################################
+cv2.circle(image_as_BGR, center, radius,color,2)
+cv2.imshow("Imagen", image_as_BGR)
+cv2.waitKey()
+
+#ellipse(img, center, axes, angle, startAngle, endAngle, color[, thickness[, lineType[, shift]]]) -> img
