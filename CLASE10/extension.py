98e2913c901748ec50d980a69c2d4e1dc77c0279
Codaerus <christianqc91@gmail.com>
2025-08-29 20:31:49 -0500

    Create extension.py

---
+++ CLASE10/extension.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+image_as_BGR = cv2.imread("image_2.png")
+image_as_GRAY = cv2.cvtColor(image_as_BGR, cv2.COLOR_BGR2GRAY)
+
+valor_umbral = 127
+valor_maximo = 255
+tipo = cv2.THRESH_BINARY
+#help(cv2.moments)
+ret, image_with_threshold = cv2.threshold(image_as_GRAY,valor_umbral, valor_maximo, tipo)
+contours, hierarchy = cv2.findContours(image_with_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#######################  CUADRO ROTADO ########################
+(x,y),(w,h), angle = cv2.minAreaRect(contours[0])
+box = cv2.boxPoints(((x,y),(w,h), angle))
+box = np.int64(box)
+######################## CUADRO RECTO ##########################
+x,y,w,h = cv2.boundingRect(contours[0])
+start_point = (x,y)
+end_point = (x+w,y+h)
+color = (0,255,0)
+######################## ENVOLTURA CONVEXA #####################
+convex_hull = cv2.convexHull(contours[0])
+######################## Relación de aspecto ###################
+aspect_ratio = w/h
+print(aspect_ratio)
+######################## EXTENSIÓN #############################
+contour_area = cv2.contourArea(contours[0]) #área del contorno
+bounding_rectangle_area = w*h #área del cuadro recto
+extent = contour_area/bounding_rectangle_area
+print(extent)
+################################################################
+cv2.drawContours(image_as_BGR,[box],-1,(0,255,255),4)
+cv2.rectangle(image_as_BGR,start_point,end_point,color,2)
+cv2.drawContours(image_as_BGR,[convex_hull],-1,(0,0,255),4)
+cv2.imshow("Imagen", image_as_BGR)
+cv2.waitKey()
